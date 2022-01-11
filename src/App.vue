3dfbc4cf64e921e2d21945fc4521d1da0bd1245e
Eric <Eric@fts.dev>
2022-01-11 17:46:25 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <MyComp/>
+</template>
+
+
+
+<script>
+import MyComp from './components/MyComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyComp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MyComp.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="myapp">
+    {{ info }}
+  </div>
+</template>
+
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'MyComp',
+
+  data () {
+    return {
+      info: null
+    }
+  },
+
+  mounted () {
+    axios
+      .get('https://api.oick.cn/yulu/api.php')
+      .then(response => (this.info = response.data))
+  }
+}
+</script>
